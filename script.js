bdda333d4649ab9055eb1369013a59e14d0699f8
nikolajovanov26 <nikolajovanov26@gmail.com>
2023-07-23 17:51:27 +0200

    show footer dropdowns only on mobile devices

--- script.js
+++ script.js
@@ -22,57 +22,70 @@ async function fetchFooterAndAddEventListener() {
         const html = await response.text();
         document.getElementById("footer").innerHTML = html;
 
+        if (screen.width < 768) {
+            document.querySelector('#footer-main-menu img').style.transform = 'rotate(-90deg)';
+            document.querySelector('#footer-my-account img').style.transform = 'rotate(-90deg)';
+
+            document.querySelector('#footer-main-menu-div').style.display = 'none';
+            document.querySelector('#footer-my-account-div').style.display = 'none';
+        }
+
         // Now that the footer content has been loaded, add the event listener.
         document.querySelector('#footer-main-menu').addEventListener('click', function () {
-            div = document.querySelector('#footer-main-menu-div');
+            if (screen.width < 768) {
+                div = document.querySelector('#footer-main-menu-div');
 
-            if (div.style.display === 'none') {
-                div.style.display = 'block';
-                document.querySelector('#footer-main-menu img').style.transform = 'rotate(0deg)';
-                setTimeout(function() {
-                    div.style.height = '100%';
-                }, 100);
-            } else {
-                document.querySelector('#footer-main-menu img').style.transform = 'rotate(-90deg)';
-                div.style.height = '0%';
-                setTimeout(function() {
-                    div.style.display = 'none';
-                }, 230)
+                if (div.style.display === 'none') {
+                    div.style.display = 'block';
+                    document.querySelector('#footer-main-menu img').style.transform = 'rotate(0deg)';
+                    setTimeout(function() {
+                        div.style.height = '100%';
+                    }, 100);
+                } else {
+                    document.querySelector('#footer-main-menu img').style.transform = 'rotate(-90deg)';
+                    div.style.height = '0%';
+                    setTimeout(function() {
+                        div.style.display = 'none';
+                    }, 230)
+                }
             }
         });
 
         document.querySelector('#footer-fast-links').addEventListener('click', function () {
-            div = document.querySelector('#footer-fast-links-div');
-            if (div.style.display === 'none') {
-                div.style.display = 'block';
-                document.querySelector('#footer-fast-links img').style.transform = 'rotate(0deg)';
-                setTimeout(function() {
-                    div.style.height = '100%';
-                }, 100);
-            } else {
-                document.querySelector('#footer-fast-links img').style.transform = 'rotate(-90deg)';
-                div.style.height = '0%';
-                setTimeout(function() {
-                    div.style.display = 'none';
-                }, 230)
+            if (screen.width < 768) {
+                div = document.querySelector('#footer-fast-links-div');
+                if (div.style.display === 'none') {
+                    div.style.display = 'block';
+                    document.querySelector('#footer-fast-links img').style.transform = 'rotate(0deg)';
+                    setTimeout(function() {
+                        div.style.height = '100%';
+                    }, 100);
+                } else {
+                    document.querySelector('#footer-fast-links img').style.transform = 'rotate(-90deg)';
+                    div.style.height = '0%';
+                    setTimeout(function() {
+                        div.style.display = 'none';
+                    }, 230)
+                }
             }
-
         });
 
         document.querySelector('#footer-my-account').addEventListener('click', function () {
-            div = document.querySelector('#footer-my-account-div');
-            if (div.style.display === 'none') {
-                div.style.display = 'block';
-                document.querySelector('#footer-my-account img').style.transform = 'rotate(0deg)';
-                setTimeout(function() {
-                    div.style.height = '100%';
-                }, 100);
-            } else {
-                document.querySelector('#footer-my-account img').style.transform = 'rotate(-90deg)';
-                div.style.height = '0%';
-                setTimeout(function() {
-                    div.style.display = 'none';
-                }, 230)
+            if (screen.width < 768) {
+                div = document.querySelector('#footer-my-account-div');
+                if (div.style.display === 'none') {
+                    div.style.display = 'block';
+                    document.querySelector('#footer-my-account img').style.transform = 'rotate(0deg)';
+                    setTimeout(function() {
+                        div.style.height = '100%';
+                    }, 100);
+                } else {
+                    document.querySelector('#footer-my-account img').style.transform = 'rotate(-90deg)';
+                    div.style.height = '0%';
+                    setTimeout(function() {
+                        div.style.display = 'none';
+                    }, 230)
+                }
             }
         });
     } catch (error) {
