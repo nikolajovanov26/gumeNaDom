ec4413d0efb4cbe6177550aa9428ca00a5737c3e
nikolajovanov26 <nikolajovanov26@gmail.com>
2023-08-02 23:43:05 +0200

    added mobile menu

--- navigation.js
+++ navigation.js
@@ -2,4 +2,21 @@ fetch("components/navigation.html")
     .then(response => response.text())
     .then(html => {
         document.getElementById("navigation").innerHTML = html;
+
+        mobileMenu = document.querySelector('[data-mobile-menu]')
+        mobileMenuOpen = document.querySelector('[data-mobile-menu-open]')
+        mobileMenuClose = document.querySelector('[data-mobile-menu-close]')
+
+        mobileMenuOpen.addEventListener('click', function () {
+            mobileMenu.classList.add('screen-m-display-flex')
+        })
+
+        mobileMenuClose.addEventListener('click', function () {
+            mobileMenu.classList.remove('screen-m-display-flex')
+        })
+
     });
+
+
+
+
