7e9ebab3043d2d40f356ff8f09ef38646935cbe2
nikolajovanov26 <nikolajovanov26@gmail.com>
2023-08-02 15:24:41 +0200

    added advanced search for mobile and improved styling on the homepage

--- advanced-search.js
+++ advanced-search.js
@@ -1,7 +1,28 @@
 toggle = document.querySelector('[data-advanced-toggle]')
+advancedButton = document.querySelector('[data-advanced-button]')
+backButton = document.querySelector('[data-advanced-back]')
+
+advancedButton.addEventListener('click', function () {
+    let event = new Event('change');
+    toggle.dispatchEvent(event);
+    document.querySelector('[data-advanced-toggle]').checked = !document.querySelector('[data-advanced-toggle]').checked
+
+    if (window.outerWidth < 992) {
+        document.querySelector('body').style.overflowY = 'hidden'
+    }
+})
+
+backButton.addEventListener('click', function () {
+    let event = new Event('change');
+    toggle.dispatchEvent(event);
+    document.querySelector('[data-advanced-toggle]').checked = 0
+
+    if (window.outerWidth < 992) {
+        document.querySelector('body').style.overflowY = 'auto'
+    }
+})
 
 toggle.addEventListener('change', function () {
     document.querySelector('.advance-filter').classList.toggle('display-none')
     document.querySelector('[data-hero-offer]').classList.toggle('display-none')
 })
-
