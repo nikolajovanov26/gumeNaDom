607a023bdc55555f4ccab1e70ab7b976393302cc
nikolajovanov26 <nikolajovanov26@gmail.com>
2023-08-03 15:27:50 +0200

    added arrow animation to compare dropdowns

--- mobile-compare.js
+++ mobile-compare.js
@@ -2,14 +2,17 @@ compareDropdowns = document.querySelectorAll('.mobile-compare-title')
 
 compareDropdowns.forEach(dropdown => {
     dropdown.addEventListener('click', function (item) {
+        dropdown.querySelector('img').style.transform =
+            dropdown.querySelector('img').style.transform === ''
+                ? 'rotateZ(180deg)'
+                : '';
+
         if(item.target.parentElement.querySelector('.mobile-compare-data')) {
             item.target.parentElement.querySelector('.mobile-compare-data').classList.toggle('display-none')
-
             return
         }
 
         parent = item.target.parentElement;
-
         if(parent.querySelector('.mobile-compare-data')) {
             item.target.parentElement.querySelector('.mobile-compare-data').classList.toggle('display-none')
         }
