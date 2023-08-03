a860fac25f52b00c2f7b8b2817975bfbf43abb91
nikolajovanov26 <nikolajovanov26@gmail.com>
2023-08-04 01:23:07 +0200

    added filter from to mobile search

--- range-slider.js
+++ range-slider.js
@@ -42,3 +42,49 @@ rangeInput.forEach(input => {
         }
     });
 });
+
+
+
+
+rangeInputMobile = document.querySelectorAll(".range-input-mobile input")
+rangeMobile = document.querySelector(".slider-mobile .progress-mobile");
+minMobile = document.querySelector('#min-price-mobile')
+maxMobile = document.querySelector('#max-price-mobile')
+priceGapMobile = 1;
+rangeInputMobile.forEach(input => {
+    input.addEventListener("input", e => {
+        let minValMobile = parseInt(rangeInputMobile[0].value),
+            maxValMobile = parseInt(rangeInputMobile[1].value);
+
+        if (minValMobile < maxValMobile) {
+            minMobile.innerHTML = minValMobile
+            maxMobile.innerHTML = maxValMobile
+        } else {
+            if (e.target.className === "range-min-mobile") {
+                minMobile.innerHTML = maxValMobile
+
+            } else {
+                maxMobile.innerHTML = minValMobile
+            }
+        }
+
+        if ((maxValMobile - minValMobile) < priceGapMobile) {
+            if (e.target.className === "range-min-mobile") {
+                rangeInputMobile[0].value = maxValMobile - priceGapMobile
+            } else {
+                rangeInputMobile[1].value = minValMobile + priceGapMobile;
+            }
+        } else {
+            rangeMobile.style.left = ((minValMobile / rangeInputMobile[0].max) * 100) + "%";
+            rangeMobile.style.right = 100 - (maxValMobile / rangeInputMobile[1].max) * 100 + "%";
+
+            minPMobile = (((minValMobile / rangeInputMobile[0].max) * 100) - 5);
+            maxPMobile = 94 - (maxValMobile / rangeInputMobile[1].max) * 100
+
+            if (minPMobile + maxPMobile < 74) {
+                minMobile.style.left = minPMobile + "%";
+                maxMobile.style.right = maxPMobile + "%";
+            }
+        }
+    });
+});
